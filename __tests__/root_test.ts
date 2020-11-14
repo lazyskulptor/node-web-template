2fcf1acc67bbc4bbba6376c6c0b30a9ac33d26ca
Park Hyeonjun <phjosh06@gmail.com>
2020-11-15 02:08:56 +0900

    Fix error on CI.

--- __tests__/root_test.ts
+++ __tests__/root_test.ts
@@ -2,6 +2,7 @@ import axios, {AxiosResponse} from 'axios';
 
 test('test cors', async (done) => {
   let response : AxiosResponse = await axios.get('http://127.0.0.1:8080/');
+  
   expect(response.headers['access-control-allow-origin']).not.toBe('*');
   done();
 });
