03e6e196848a14883c8ad9d2c8ccf1f26e5f588a
Park Hyeonjun <phjosh06@gmail.com>
2020-11-18 22:35:36 +0900

    localization is applied.

--- src/config/configure.ts
+++ src/config/configure.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import * as I18n from "i18n";
 
 export = (app : express.Application) : void => {
   app.set("views", path.join(__dirname, "../views"));
   app.set("view engine", "ejs");
-
+  
+  I18n.configure({
+    locales: ['en', 'ko'],
+    directory: path.join(__dirname, '../locales'),
+    queryParameter: 'lang'
+  });
+  
+  app.use(I18n.init);
   app.use(bodyParser.urlencoded({ extended: true }));
 }
